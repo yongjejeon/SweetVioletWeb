3c616fa0a2d538018efc3da9dea9b4429c07d1fc
Yongje <159231081+yongjejeon@users.noreply.github.com>
2024-11-06 12:22:09 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+
 const Header = ({ title, navItems }) => {
   return (
     <header>
