bbcec93fef08b53aa98df594ce90be6c28406bfd
Yongje Jeon <yj2082@nyu.edu>
2024-11-06 13:33:18 -0500

    changed preferences.js and css for it

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,6 +9,12 @@ const Button = ({ label, onClick, variant = 'primary', disabled = false }) => {
       padding: '10px 20px',
       cursor: 'pointer',
       border: 'none',
+      borderRadius: '5px', // Rounded corners
+      width: '100%',
+      maxWidth: '250px', // Limit button width for centering
+      textAlign: 'center',
+      marginTop: '30px',
+      fontSize: '20px',
     },
     secondary: {
       backgroundColor: '#fff',
@@ -16,13 +22,25 @@ const Button = ({ label, onClick, variant = 'primary', disabled = false }) => {
       padding: '10px 20px',
       cursor: 'pointer',
       border: '1px solid #333',
+      borderRadius: '5px',
+      width: '100%',
+      maxWidth: '250px',
+      textAlign: 'center',
+      marginTop: '30px',
+      fontSize: '20px',
     },
     disabled: {
-      backgroundColor: '#ccc', // Disabled background color
-      color: '#666',            // Disabled text color
+      backgroundColor: '#ccc',
+      color: '#666',
       padding: '10px 20px',
-      cursor: 'not-allowed',     // Change cursor to indicate non-clickable
+      cursor: 'not-allowed',
       border: 'none',
+      borderRadius: '5px',
+      width: '100%',
+      maxWidth: '250px',
+      textAlign: 'center',
+      marginTop: '30px',
+      fontSize: '20px',
     }
   };
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -4,13 +4,15 @@ import React from 'react';
 const Input = ({ label, value, onChange, placeholder }) => {
   return (
     <div>
-      <label>{label}</label>
+      <label style={{ fontSize: '20px', textAlign: 'center', display: 'block', marginTop: '10px' }}>
+  {label}
+      </label>
       <input
         type="number"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
-        style={{ marginLeft: '10px', padding: '5px' }}
+        style={{ marginLeft: '10px', padding: '5px', fontSize: '25px', marginTop: '10px'}}
       />
     </div>
   );
